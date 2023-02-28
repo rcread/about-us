16675b210bec97a5af9a8c8214646b9cc5d03e68
Rachel Read <rachelread@thecambiumgroup.co.uk>
2023-02-28 11:14:03 +0000

    dropdown function working, divider needs to be readded in

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 let today = new Date();
 let yyyy = today.getFullYear();
-
 document.querySelector("#copyrightYear").innerHTML = yyyy;
+
+// const footerMenu = document.querySelector(".heading-container");
+// const footerMenuList = document.querySelector(".footer__menu-list")
+
+// footerMenu.addEventListener("click", (expandMenu) => {
+//     function expandMenu() {
+//         footerMenu.classList.toggle("active");
+//         footerMenuList.classList.toggle("active");
+//     }
+// })
+
+let expandMenu = document.getElementsByClassName("heading-container");
+let i;
+
+for (i = 0; i < expandMenu.length; i++) {
+  expandMenu[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let footerMenuList = this.nextElementSibling;
+    if (window.innerWidth < 768 & footerMenuList.style.display === "block") {
+      footerMenuList.style.display = "none";
+    } else {
+      footerMenuList.style.display = "block";
+    }
+  });
+}
