d80f1943602829089e4f6167aade4785091273ca
Rachel Read <rachelread@thecambiumgroup.co.uk>
2023-03-02 11:26:44 +0000

    removed comments from js file

--- main.js
+++ main.js
@@ -6,20 +6,6 @@ let expandMenu = document.getElementsByClassName("open-menu");
 let arrow = document.getElementsByClassName("arrow-down");
 let i;
 
-// for (i = 0; i < expandMenu.length; i++) {
-//   expandMenu[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     this.querySelector(".arrow-down").classList.toggle("arrow-active");
-//     let footerMenuList = this.nextElementSibling;
-//     if (window.matchMedia ('(width: <768px)') & footerMenuList.style.display === "block") {
-//       footerMenuList.style.display = "none";
-//     } 
-//     else {
-//       footerMenuList.style.display = "block";
-//     }
-//   });
-// }
-
 const mediaQueryList = window.matchMedia("(max-width: 768px)");
 
 const toggleFooterMenu = (e) => {
