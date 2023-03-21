decfd9d7a7e12b178d3264b0d7f13288b43d3174
rcread <113596529+rcread@users.noreply.github.com>
2023-03-21 15:16:57 +0000

    F/footer (#7)
    * initial commit for footer
    
    * added article section, still figuring out mobile design for footer details
    
    * updated except copyright should be right aligned in >md
    
    * updated footer
    
    * added js date for copyright year
    
    * dropdown function working, divider needs to be readded in
    
    * added ::after pseudoelement for menu border
    
    * arrow rotation update, not working
    
    * fixes required on footer
    
    * matchMedia now in place, expanded menu doesn't close now
    
    * removed comments from main.js
    
    * added socials section for instagram
    
    * menu expand function working in mobile design
    
    * relative positioning added to open menu
    
    * open menu after fixed
    
    * removed comments from js file
    
    * scroll to top
    
    * changes to html
    
    * footer updates
    
    * team and history added
    
    * scss updates
    
    * socials added
    
    * gallery details
    
    * scroll to top added
    
    * js updates
    
    * js updates
    
    * gallery fixed
    
    * added mailto and tel details
    
    * background modifier
    
    * updated gallery to list
    
    * updated per PR comments
    
    * small updates
    
    * removed unnecessary scss
    
    * updated all files with prettier
    
    * changed scrollBtn style on scroll
    
    * scroll functionality working
    
    * scroll button changes to position
    
    * added favicon
    
    * changed title

---
+++ main.js
@@ -0,0 +1,63 @@
+let today = new Date();
+let yyyy = today.getFullYear();
+document.querySelector("#copyrightYear").innerHTML = yyyy;
+
+const expandMenu = document.getElementsByClassName("open-menu");
+const arrow = document.getElementsByClassName("arrowDown");
+let i;
+
+const mediaQueryList = window.matchMedia("(max-width: 768px)");
+
+const toggleFooterMenu = (e) => {
+  for (i = 0; i < expandMenu.length; i++) {
+    const menuLists = document.querySelectorAll(".footer__menu-list--hidden");
+    const arrows = document.querySelectorAll(".arrowDown");
+
+    menuLists.forEach((menuList) => {
+      menuList.style.display = "none";
+    });
+
+    arrows.forEach((arrow) => {
+      arrow.classList.remove("arrow--active");
+    });
+
+    if (!e.matches) {
+      menuLists.forEach((menuList) => {
+        menuList.style.display = "block";
+      });
+    }
+
+    expandMenu[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      this.querySelector(".arrowDown").classList.toggle("arrow--active");
+      let footerMenuList = this.nextElementSibling;
+
+      if (e.matches) {
+        if (footerMenuList.style.display === "block") {
+          footerMenuList.style.display = "none";
+        } else {
+          footerMenuList.style.display = "block";
+        }
+      }
+    });
+  }
+};
+
+toggleFooterMenu(mediaQueryList);
+mediaQueryList.addEventListener("change", toggleFooterMenu);
+
+let scrollButton = document.getElementById("scrollBtn");
+window.onscroll = function () { scrollFunction() };
+scrollButton.style.display = "none";
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
